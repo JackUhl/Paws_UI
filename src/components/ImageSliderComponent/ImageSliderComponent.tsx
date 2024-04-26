d9ca61d334049c94c4a7b3173d29b7a718f74fe7
Jonathon-Ford <61033062+Jonathon-Ford@users.noreply.github.com>
2024-04-25 22:33:27 -0400

    Animate the arrows and allow for picture change

--- src/components/ImageSliderComponent/ImageSliderComponent.tsx
+++ src/components/ImageSliderComponent/ImageSliderComponent.tsx
@@ -6,11 +6,37 @@ export default function ImageSliderComponent(props: IImageSliderComponent) {
 
     const [imgNum, setImgNum] = useState(props.filePaths.length);
     const [curImgIndex, setCurImgIndex] = useState(0);
+    const [hover, setHover] = useState(false);
+
+    const changeCurImg = (newIndex : number) => {
+        if (newIndex == imgNum){
+            setCurImgIndex(0);
+        }
+        else if (newIndex < 0){
+            setCurImgIndex(imgNum - 1);
+        }
+        else {
+            setCurImgIndex(newIndex);
+        }
+    }
 
   return (
     <>
-      <div>
+      <div 
+      onMouseEnter={ () => {setHover(true);}}
+      onMouseLeave={ () => {setHover(false);}}
+      >
         <div className='imageSliderContainer' id='imageSlider'>
+            <span className={`arrowSpanLeft ${hover ? '' : 'fade-out'}`}> 
+                <i className='arrow left'
+                onClick={() => {changeCurImg(curImgIndex-1)}}
+                /> 
+            </span>
+            <span className={`arrowSpanRight ${hover ? '' : 'fade-out'}`}>
+                <i className='arrow right'
+                onClick={() => {changeCurImg(curImgIndex+1)}}
+                /> 
+            </span>
             <img className='imageSliderImg' id= {'selectedImg' + curImgIndex } src={props.filePaths[curImgIndex]}/>
         </div>
       </div>
--- src/models/constants/HomePageImgSliderConstants.tsx
+++ src/models/constants/HomePageImgSliderConstants.tsx
@@ -1,4 +1,6 @@
 export const HomePageImages: string[] = [
     `src\\assets\\HomePageImageSlider\\Cat1.jpg`,
-    `src\\assets\\HomePageImageSlider\\Cat2.jpg`
+    `src\\assets\\HomePageImageSlider\\Cat2.jpg`,
+    `src\\assets\\HomePageImageSlider\\Cat3.jpg`,
+    `src\\assets\\HomePageImageSlider\\Cat4.jpg`
 ]
