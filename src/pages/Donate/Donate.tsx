e51ecadc9b93c400b983b262bf4dae53801614d3
Jackson Uhl <jackson.g.uhl@gmail.com>
2024-05-17 20:01:56 -0400

    Reword

--- src/pages/Donate/Donate.tsx
+++ src/pages/Donate/Donate.tsx
@@ -16,7 +16,7 @@ export default function Donate() {
             ></TitleBanner>
             <div className='maxContentWrapper'>
                 <div className='flexColumn contentBox'>
-                    <h2>Make a Pawsitive Impact Today!</h2>
+                    <h2>Make an Impact Today!</h2>
                     <p>Looking for a way to make a pawsitive impact? Support PAWS, the Pawsome Animal Welfare Society, and help us continue our mission! With your generous donations and unwavering support, we can create a warm and nurturing environment where all pets feel safe and loved.</p>
                     <p>Your contributions are the backbone of our mission. With the majority of donations allocated towards essential pet supplies and vet bills, every dollar you donate directly impacts the well-being and care of our rescue pets, ensuring they receive the support they need to thrive.</p>
                 </div>
@@ -25,11 +25,11 @@ export default function Donate() {
                     <div className='flexRow flexGap'>
                         <div className='flexColumn alignCenter donateExamplesRow'>
                             <img src={yalina} alt="yalina" className='donateExampleImage' />
-                            <p>We're able to afford medical procedures such as providing life-saving heartworm treatment for one of our rescues, Yalina, ensuring her journey towards a happy and healthy life.</p>
+                            <p>With your help, we are able to afford medical procedures such as providing life-saving heartworm treatment for one of our rescues, Yalina, ensuring her journey towards a happy and healthy life.</p>
                         </div>
                         <div className='flexColumn alignCenter donateExamplesRow'>
                             <img src={winston} alt="winston" className='donateExampleImage' />
-                            <p>We're able to afford services such as hospice care which offered comfort and dignity for one of our rescues, Winston, ensuring he felt loved and supported.</p>
+                            <p>Donations allow us to afford services such as hospice care which offered comfort and dignity for one of our rescues, Winston, ensuring he felt loved and supported.</p>
                         </div>
                         <div className='flexColumn alignCenter donateExamplesRow'>
                             <img src={dogToys} alt="dog toys" className='donateExampleImage' />
@@ -40,7 +40,7 @@ export default function Donate() {
                 <div className='flexColumn contentBox'>
                     <h2>Donate</h2>
                     <div className='flexRow alignCenter flexGap'>
-                        <p className='donateArea'>Any and all donations ares appreciated. Rest assured that your genrous contributions will be respectfully allocated towards bettering the lives of our rescues</p>
+                        <p className='donateArea'>Any and all donations are appreciated. Rest assured that your genrous contributions will be respectfully allocated towards bettering the lives of our rescues</p>
                         <LinkButtonComponent
                             linksToInternalRoute={false}
                             route={PaypalUrl}
