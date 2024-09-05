c718f3e9e3642f0ab24724ae9ab85f0df2c19a98
Jackson Uhl <jackson.g.uhl@gmail.com>
2024-09-04 23:12:14 -0400

    Add descriptors

--- src/pages/PetDetails/PetDetails.tsx
+++ src/pages/PetDetails/PetDetails.tsx
@@ -8,6 +8,9 @@ import { IsMobileContext } from "../../contexts/IsMobileContext";
 import { IPetDetails } from "./IPetDetails";
 import LinkButtonComponent from "../../components/LinkButtonComponent/LinkButtonComponent";
 import petfinderIcon from "../../assets/petfinderIcon.png"
+import checkMarkIcon from "../../assets/checkMarkIcon.svg"
+import cancelIcon from "../../assets/cancelIcon.svg"
+import questionMarkIcon from "../../assets/questionMarkIcon.svg"
 
 export default function PetDetails(props: IPetDetails) {
     const isMobile = useContext<boolean>(IsMobileContext);
@@ -28,6 +31,22 @@ export default function PetDetails(props: IPetDetails) {
     const fullBreed = `${props.adoptablePetInfo.age} ${props.adoptablePetInfo.breeds.primary + (props.adoptablePetInfo.breeds.mixed ? ` Mix` : "")}`;
     const genderStatus = `${props.adoptablePetInfo.gender} ${props.adoptablePetInfo.attributes.spayed_neutered ? " (Fixed)" : " (Not Fixed)"}`;
 
+    const getPropertyImage = (property: boolean | null): React.ReactNode => {
+        let icon;
+        
+        if(property == true) {
+            icon = checkMarkIcon;
+        }
+        else if(property == false) {
+            icon = cancelIcon;
+        }
+        else {
+            icon = questionMarkIcon
+        }
+
+        return <img src={icon} className="icon" />
+    }
+
     return (
         <div className="petDetails">
             <div className="bottomSpacing">
@@ -45,6 +64,37 @@ export default function PetDetails(props: IPetDetails) {
                         <h2>{props.adoptablePetInfo.name}</h2>
                         <p>{fullBreed} | {genderStatus}</p>
                     </div>
+
+                    <div className="bottomSpacing">
+                        <p>More about {props.adoptablePetInfo.name}</p>
+                        <table>
+                            <tr>
+                                <td>Good with children</td>
+                                <td>{getPropertyImage(props.adoptablePetInfo.environment.children)}</td>
+                            </tr>
+                            <tr>
+                                <td>Good with dogs</td>
+                                <td>{getPropertyImage(props.adoptablePetInfo.environment.dogs)}</td>
+                            </tr>
+                            <tr>
+                                <td>Good with cats</td>
+                                <td>{getPropertyImage(props.adoptablePetInfo.environment.cats)}</td>
+                            </tr>
+                            <tr>
+                                <td>House trained</td>
+                                <td>{getPropertyImage(props.adoptablePetInfo.attributes.house_trained)}</td>
+                            </tr>
+                            <tr>
+                                <td>Special needs</td>
+                                <td>{getPropertyImage(props.adoptablePetInfo.attributes.special_needs)}</td>
+                            </tr>
+                            <tr>
+                                <td>Shots current</td>
+                                <td>{getPropertyImage(props.adoptablePetInfo.attributes.special_needs)}</td>
+                            </tr>
+                        </table>
+                    </div>
+
                     <p>{props.adoptablePetInfo.description}</p>
                     <LinkButtonComponent
                         linksToInternalRoute={false}
