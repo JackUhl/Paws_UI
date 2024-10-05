1b8a2a0a0138f72311d17197144cf82c50565661
Jackson Uhl <jackson.g.uhl@gmail.com>
2024-10-04 23:16:06 -0400

    Fix EmailService

--- src/services/EmailService/EmailService.ts
+++ src/services/EmailService/EmailService.ts
@@ -1,34 +1,30 @@
-import axios, { AxiosHeaders, AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { VolunteerApplicationRequest } from "../../models/DTOs/VolunteerApplicationRequest";
 import { FosterApplicationApiRoute, VolunteerApplicationApiRoute } from "./EmailRoutes";
 import { FosterApplicationRequest } from "../../models/DTOs/FosterApplicationRequest";
 
 export class EmailService {
-    public static PostFosterApplication(fosterApplicationRequest: FosterApplicationRequest) {
-        const config: AxiosRequestConfig = {
-            headers: {
-                "Content-Type": "text/plain; charset=utf-8",
-            }
-        }
-        
+    public static PostFosterApplication(fosterApplicationRequest: FosterApplicationRequest) {       
         return axios.post<FosterApplicationRequest>(
             import.meta.env.VITE_PAWS_API_END_POINT + FosterApplicationApiRoute,
             fosterApplicationRequest,
-            config
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
         );
     }
 
     public static PostVolunteerApplication(volunteerApplicationRequest: VolunteerApplicationRequest) {
-        const config: AxiosRequestConfig = {
-            headers: {
-                "Content-Type": "text/plain; charset=utf-8",
-            }
-        }
-        
         return axios.post<VolunteerApplicationRequest>(
             import.meta.env.VITE_PAWS_API_END_POINT + VolunteerApplicationApiRoute,
             volunteerApplicationRequest,
-            config
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
         );
     }
 }
